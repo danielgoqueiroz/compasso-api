1486f75c0155c59d45401db29c2698bcbd930568
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-03-28 16:12:30 -0300

    validações de get

--- src/main/java/com/krys/compassoapi/compassoapi/controller/UserController.java
+++ src/main/java/com/krys/compassoapi/compassoapi/controller/UserController.java
@@ -28,34 +28,41 @@ import com.krys.compassoapi.compassoapi.model.User;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-		
+
 	Set<User> users = new HashSet<User>();
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON)
-	public ResponseEntity<?> getArtists(@RequestParam(required = false, name="name") String name, @RequestParam(required = false, name="id") Long id) {
-		Optional<User> findById = users.stream().filter(u -> id == u.getId()).findFirst();
-		if (findById.isPresent()) {
-			User user = findById.get();
-			return ResponseEntity.ok(user);
+	public ResponseEntity<?> getArtists(@RequestParam(required = false, name = "name") String name,
+			@RequestParam(required = false, name = "id") Long id) {
+
+		if (id != null) {
+			Optional<User> findById = users.stream().filter(u -> id == u.getId()).findFirst();
+			if (findById.isPresent()) {
+				User user = findById.get();
+				return ResponseEntity.ok(user);
+			}
 		}
-		
-		Optional<User> findByName = users.stream().filter(u -> name.contentEquals(u.getName())).findFirst();
-		if (findByName.isPresent()) {
-			User user = findByName.get();
-			return ResponseEntity.ok(user);
+
+		if (!Strings.isNullOrEmpty(name)) {
+
+			Optional<User> findByName = users.stream().filter(u -> name.contentEquals(u.getName())).findFirst();
+			if (findByName.isPresent()) {
+				User user = findByName.get();
+				return ResponseEntity.ok(user);
+			}
 		}
 		return ResponseEntity.ok(users);
 	}
-	
+
 	@PostMapping(consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
 	public ResponseEntity<?> save(@RequestBody User user) {
-		
+
 		validate(user);
 		addUser(user);
-		
-		return ResponseEntity.ok(user) ;
+
+		return ResponseEntity.ok(user);
 	}
-	
+
 	private void addUser(User user) {
 		long id = new Random().nextLong();
 		user.setId(id < 0 ? id * -1 : id);
@@ -63,35 +70,37 @@ public class UserController {
 	}
 
 	private void validate(User user) {
-		
+
 		if (Strings.isNullOrEmpty(String.valueOf(user.getId()))) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Foi enviado um usuário com o valor de id preenchido. Para salvar um novo usuário deve-se enviar sem o valor de id.");
-		} 
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Foi enviado um usuário com o valor de id preenchido. Para salvar um novo usuário deve-se enviar sem o valor de id.");
+		}
 		if (Strings.isNullOrEmpty(user.getCpf()) || user.getCpf().length() < 8) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O valor de CPF deve conter dados e deve ser maior que 8 caracteres.");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"O valor de CPF deve conter dados e deve ser maior que 8 caracteres.");
+		}
+		if (Strings.isNullOrEmpty(user.getCpf()) || user.getPassword().length() < 8) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"O valor de PASSWORD deve conter dados e deve ser maior que 8 caracteres.");
 		}
 		if (Strings.isNullOrEmpty(user.getName()) || user.getName().length() < 5) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O valor de NOME deve conter dados e deve ser maior que 5 caracteres.");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"O valor de NOME deve conter dados e deve ser maior que 5 caracteres.");
 		}
 		Pattern patterEmail = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 		Matcher matcher = patterEmail.matcher(user.getEmail());
-		if (Strings.isNullOrEmpty(user.getEmail()) || !matcher.find()) { 
+		if (Strings.isNullOrEmpty(user.getEmail()) || !matcher.find()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O valor de EMAIL não é válido.");
 		}
-		
-		Optional<User> findFirst = users.stream().filter(u -> 
-			user.getName().contentEquals(u.getName())
-			|| user.getCpf().contentEquals(u.getCpf())
-			|| user.getEmail().contentEquals(u.getEmail())
-				)
+
+		Optional<User> findFirst = users
+				.stream().filter(u -> user.getName().contentEquals(u.getName())
+						|| user.getCpf().contentEquals(u.getCpf()) || user.getEmail().contentEquals(u.getEmail()))
 				.findFirst();
 		if (findFirst.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.CONFLICT);
 		}
-		
-	}
 
-	
-	
+	}
 
 }
